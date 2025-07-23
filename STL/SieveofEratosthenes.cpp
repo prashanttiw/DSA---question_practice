20f116a528df9e60e64529c3e82e18c19e9c8b76
Prashant93156203 <115913516+Prashant93156203@users.noreply.github.com>
2025-07-23 23:30:33 +0530

    day-2

--- STL/SieveofEratosthenes.cpp
+++ STL/SieveofEratosthenes.cpp
@@ -1,3 +1,31 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+
+int countPrimes(int n) {
+        vector<bool> vec(n, true);
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+if(vec[i]){
+                count++;
+            }
+            for (int j = i*2; j < n; j=i+j) {
+                
+                    vec[j]=false;
+                
+            }
+        }
+
+       
+
+        return count;
+    }
+
+
+
+int main(){
+cout<< countPrimes(500)<<endl;
+    return 0;
+}
